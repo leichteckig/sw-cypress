0a9e2a57d2e3f8f20b3636b0d0d0d7b52b5325dc
Thomas Lohner <tl@scale.sc>
2019-04-12 10:54:31 +0200

    remove fixture in favor for api command

--- cypress/integration/storefront/product/product-open.spec.js
+++ cypress/integration/storefront/product/product-open.spec.js
@@ -1,11 +1,9 @@
 describe('Product: Open product page', function () {
   this.product = {};
-  beforeEach(() => {
-    cy.fixture('product').as('product');
-  });
 
     it('check if product is visible', function () {
         cy.getRandomProductInformationForCheckout().then((product) => {
+ 
             cy.visit(product.url);
 
             cy.get('.product-detail-name').contains(product.name);
@@ -15,12 +13,11 @@ describe('Product: Open product page', function () {
 
     it('searches product in search bar', function () {
         cy.getRandomProductInformationForCheckout().then((product) => {
+ 
             cy.visit('/');
 
             cy.get('input[type="search"]').type(product.name);
-
             cy.get('ul.search-suggest-container .result-link:first').click();
-
             cy.url().should('contain', product.url);
             cy.get('.product-detail-name').contains(product.name);
         });
