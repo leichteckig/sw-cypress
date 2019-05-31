fe9cb9d3f0b40f67630ab4f9f8f55a1ba572a084
leichteckig <29896429+leichteckig@users.noreply.github.com>
2019-05-31 14:52:39 +0200

    Fix cypress shopware theme for version 3.3.1 and other dependencies

--- cypress/support/commands/system-commands.js
+++ cypress/support/commands/system-commands.js
@@ -15,7 +15,8 @@ Cypress.Commands.add("activateShopwareTheme", () => {
         return
     }
 
-    cy.readFile(themeFilename, { log: false }).then(css => {
+    // TODO: Substring workaround for Cypress issue #4352, please remove asap
+    cy.readFile(themeFilename.substring(1), { log: false }).then(css => {
         $head.append(
             `<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/brands.css" integrity="sha384-i2PyM6FMpVnxjRPi0KW/xIS7hkeSznkllv+Hx/MtYDaHA5VcF0yL3KVlvzp8bWjQ" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/fontawesome.css" integrity="sha384-sri+NftO+0hcisDKgr287Y/1LVnInHJ1l+XC7+FOabmTTIK0HnE2ID+xxvJ21c5J" crossorigin="anonymous">`
